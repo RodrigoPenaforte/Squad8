19df050372b12f208c8aafdead3e6cb58e9ae66a
drielisonlopes <drielisonl@gmail.com>
2022-01-05 16:58:40 -0300

    MVC / CRUD

---
+++ Mulher-Presente/Mulher-Presente/Data/VitimaUserContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace Mulher_Presente.Data
+{
+    public class VitimaUserContext : DbContext
+    {
+        public VitimaUserContext(DbContextOptions<VitimaUserContext> opt) : base(opt)
+        {
+
+        }
+            public DbSet<Models.VitimaUser> VitimaUser { set; get; }
+
+            public DbSet<Models.ParceirosUser> ParceirosUser { set; get; }
+    }
+    
+}
+
+ 
+           
+    
---
+++ Mulher-Presente/Mulher-Presente/Models/VitimaUser.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mulher_Presente.Models
+{
+    public class VitimaUser
+    {
+        [Key]
+        [Required]
+        public string ID_vitima { get; set; }
+        [Required]
+        public string Nome { get; set; }
+        [Required]
+        public string Telefone { get; set; }
+        [Required]
+
+        public string Email { get; set; }
+
+        // FK edição
+
+        [Required]
+
+        public int ParceirosUserID_parceiro { get; set; }
+
+        public ParceirosUser ParceirosUser { get; set; }
+
+
+    } 
+}
