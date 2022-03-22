9766f9e3217eb55d939e650a8ffb7bfab9d23e3c
drielisonlopes <drielisonl@gmail.com>
2022-03-22 15:31:16 -0300

    implementacao do Login

--- Mulher_Presente/Controllers/HomeController.cs
+++ Mulher_Presente/Controllers/HomeController.cs
@@ -38,6 +38,16 @@ namespace Mulher_Presente.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult EsqueceuSenha()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
